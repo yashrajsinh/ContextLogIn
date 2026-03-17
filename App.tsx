879d44882ee8d08c7cdbad57d210ba95d2945113
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-17 16:50:12 +0530

    Tabs assign
    - added context in project
    - tabs assigned

--- App.tsx
+++ App.tsx
@@ -1,24 +1,42 @@
-import { StyleSheet, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
 //Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//Bottom nav
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //screens
 import LogInScreen from './src/screens/LogInScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
+import UserProfile from './src/screens/UserProfile';
+
+//Context
+import Userprovider from './src/context/UserProvider';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={WelcomeScreen} />
+      <Tab.Screen name="Profile" component={UserProfile} />
+    </Tab.Navigator>
+  );
+}
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={LogInScreen} />
-        <Stack.Screen name="Details" component={WelcomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Userprovider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={LogInScreen} />
+          <Stack.Screen
+            name="MainTabs"
+            component={MainTabs}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Userprovider>
   );
 }
 
---
+++ src/context/UserProvider.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <AuthContext.Provider
+      value={{
+        username,
+        setUsername,
+        password,
+        setPassword,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
